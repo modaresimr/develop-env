1104e613cdd612f0ebce577fb07f3d3b084ff31f
Ali <modaresimr@gmail.com>
2022-10-31 04:03:34 +0100

    fix bug

--- src/startup/my-startup.sh
+++ src/startup/my-startup.sh
@@ -3,7 +3,7 @@ echo "running my startup"
 CONTAINERS_FOLDER=/env
 
 if [ ! -f $CONTAINERS_FOLDER/run-continers.sh ]; then
-    cp $STARTUPDIR/run-continers.sh $CONTAINERS_FOLDER/run-continers.sh
+    cp run-continers.sh $CONTAINERS_FOLDER/run-continers.sh
 fi
 
 (cd $CONTAINERS_FOLDER; bash $CONTAINERS_FOLDER/run-continers.sh)
