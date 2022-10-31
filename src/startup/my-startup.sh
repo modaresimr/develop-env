965dfa4afe999d5ee6ed91f55563c494f76f64a2
Ali <modaresimr@gmail.com>
2022-10-31 09:40:24 +0100

    update restart docker

--- src/startup/my-startup.sh
+++ src/startup/my-startup.sh
@@ -6,11 +6,20 @@ if [ ! -f $CONTAINERS_FOLDER/run-continers.sh ]; then
     cp run-continers.sh $CONTAINERS_FOLDER/run-continers.sh
 fi
 
-
+i=0
 until docker ps;do
-    echo 'my startup-> waiting for docker to be up'
+    
+    echo "my startup-> waiting for docker to be up $i"
+    if [ $(($i % 10)) == 0 ];then 
+        kill -9 $(cat /var/run/docker.pid)
+        sleep 3
+        rm /var/run/docker.pid
+        supervisorctl start dockerd
+    fi
     sleep 10
+    i=$((i+1))
 done
+
 (cd $CONTAINERS_FOLDER; bash $CONTAINERS_FOLDER/run-continers.sh)
 
 
