4be22e938fc4fe7ee3158ce509590f3af67d8225
Ali <modaresimr@gmail.com>
2022-10-31 03:54:14 +0100

    change to env folder

--- src/startup/my-startup.sh
+++ src/startup/my-startup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "running my startup"
+CONTAINERS_FOLDER=env
+
 if [ ! -f $CONTAINERS_FOLDER/run-continers.sh ]; then
     cp $STARTUPDIR/run-continers.sh $CONTAINERS_FOLDER/run-continers.sh
 fi
