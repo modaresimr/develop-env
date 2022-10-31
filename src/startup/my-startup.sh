e4143bdecad59184e83db8b2b163c75a2ca22699
Ali <modaresimr@gmail.com>
2022-10-31 08:16:29 +0100

    wait until docker is up

--- src/startup/my-startup.sh
+++ src/startup/my-startup.sh
@@ -6,6 +6,11 @@ if [ ! -f $CONTAINERS_FOLDER/run-continers.sh ]; then
     cp run-continers.sh $CONTAINERS_FOLDER/run-continers.sh
 fi
 
+
+until docker ps;do
+    echo 'my startup-> waiting for docker to be up'
+    sleep 10
+done
 (cd $CONTAINERS_FOLDER; bash $CONTAINERS_FOLDER/run-continers.sh)
 
 
--- src/startup/run-continers.sh
+++ src/startup/run-continers.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 IMAGE_FOLDERS="" #image folders to run automatically
 
-sleep 10
-
 for folder in $IMAGE_FOLDERS; do
     (cd $folder;docker-compose up -d)
 done
